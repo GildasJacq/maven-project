92b9b1b011511d6c12268f2beb9aa53eda294f13
GildasJacq <61284749+GildasJacq@users.noreply.github.com>
2021-05-31 16:36:46 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
   
 /**
- * @param someone nom de la personne.
- * @return bonjour en String.
+ * @param someone nom de la personne
+ * @return bonjour en String
  */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
